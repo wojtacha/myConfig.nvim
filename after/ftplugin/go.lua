443e4f90c5f628608168c489e44208b1ea2b7d6b
wojtacha <wojtaszewskim91@gmail.com>
2025-03-31 07:28:46 +0000

    last touch fixes

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -3,7 +3,3 @@ local set = vim.opt_local
 set.expandtab = false
 set.tabstop = 4
 set.shiftwidth = 4
-
--- vim.keymap.set("n", "<space>td", function()
---   require("dap-go").debug_test()
--- end, { buffer = 0 })
