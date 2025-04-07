a602bcfdbea8b12187cab09376d29ff0cfdd9bc0
wojtacha <michal.wojtaszewski@beekeeper.io>
2025-04-07 15:02:18 +0200

    polished gitsigns

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -1,27 +1,19 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config =
-        function()
-          require('gitsigns').setup {
-            signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
-            numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-            linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-            word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-          }
-        end
-  },
-
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
       },
+
+      signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     },
   },
 
